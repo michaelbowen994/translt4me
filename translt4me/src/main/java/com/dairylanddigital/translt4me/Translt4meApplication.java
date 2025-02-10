18213ebbbe4b3fc5177c80895efe220bd1d7b22a
michaelbowen <michael.bowen994@gmail.com>
2025-02-10 01:44:46 -0600

    initialization

---
+++ translt4me/src/main/java/com/dairylanddigital/translt4me/Translt4meApplication.java
@@ -0,0 +1,13 @@
+package com.dairylanddigital.translt4me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Translt4meApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Translt4meApplication.class, args);
+	}
+
+}
---
+++ translt4me/src/test/java/com/dairylanddigital/translt4me/Translt4meApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dairylanddigital.translt4me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Translt4meApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
